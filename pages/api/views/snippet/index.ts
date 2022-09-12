35c80b06f8576a9feb0ea5c7d114a68682eba35e
Tony Pettigrew <tonypdev@Tonys-MacBook-Pro.local>
2022-09-12 10:09:10 -0700

    feat: Add snippet view count to dashboard metrics

--- pages/api/views/snippet/index.ts
+++ pages/api/views/snippet/index.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         count: true,
       },
     });
-
+    console.log(totalViewCount, "TVC");
     return res.status(200).json({ total: totalViewCount._sum.count?.toString() });
   } catch (e: any) {
     return res.status(500).json({ message: e?.message || "Server error" });
